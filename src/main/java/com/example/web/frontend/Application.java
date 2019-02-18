73f7f39621e4de2e0d491922b8069a583ca22682
DaShaun Carter <dashaun@dashaun.com>
2019-02-17 20:38:26 -0600

    remove eureka and upgrade to spring boot 1.5.19

--- src/main/java/com/example/web/frontend/Application.java
+++ src/main/java/com/example/web/frontend/Application.java
@@ -2,10 +2,8 @@ package com.example.web.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@EnableDiscoveryClient
 @RefreshScope
 @SpringBootApplication
 public class Application {
